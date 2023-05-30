2f9de651e4d44b646e2363e47184291cf81cbd8f
Iury Assunção <iuryifba@gmail.com>
2023-05-30 18:11:18 -0300

    feat: inserting execution handling

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -3,10 +3,12 @@ import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressEntity } from './entities/address.entity';
+import { UserModule } from '../user/user.module';
+import { CityModule } from '../city/city.module';
 
 @Module({
   controllers: [AddressController],
   providers: [AddressService],
-  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  imports: [TypeOrmModule.forFeature([AddressEntity]), UserModule, CityModule],
 })
 export class AddressModule {}
